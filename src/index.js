f1855ce79872616a93a22003035b3763c483544e
nzambello <nzambello@protonmail.com>
2020-07-24 12:12:39 +0200

    compatibility with volto 7

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import CookieBanner from './CookieBannerContainer'
-import { GET_COOKIE_CONSENT_INFOS, getCookieConsentInfos } from './actions'
-import { cookieConsentReducer } from './reducers'
+import { cookieConsentReducer } from 'volto-cookie-banner/reducers'
 
-export { CookieBanner, GET_COOKIE_CONSENT_INFOS, getCookieConsentInfos, cookieConsentReducer }
+export CookieBanner from 'volto-cookie-banner/CookieBannerContainer'
+export { GET_COOKIE_CONSENT_INFOS, getCookieConsentInfos } from 'volto-cookie-banner/actions'
+
+export default config => {
+  config.addonReducers = {
+    ...config.addonReducers,
+    cookieConsentInfos: cookieConsentReducer,
+  }
+
+  return config
+}
