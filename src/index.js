1bbd2ee97a5f8765e85e6acedcf245021cff437f
nzambello <nzambello@protonmail.com>
2020-07-24 12:20:44 +0200

    fix exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { cookieConsentReducer } from 'volto-cookie-banner/reducers'
 
-export CookieBanner from 'volto-cookie-banner/CookieBannerContainer'
+export { default as CookieBanner } from 'volto-cookie-banner/CookieBannerContainer'
 export { GET_COOKIE_CONSENT_INFOS, getCookieConsentInfos } from 'volto-cookie-banner/actions'
 
 export default config => {
