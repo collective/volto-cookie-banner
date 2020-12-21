1e91dac877c78828825e7de52bc761c91ee33cd2
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-12-21 17:53:20 +0100

    added flattenHTMLToAppURL to display content

--- src/CookieBanner.js
+++ src/CookieBanner.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import CookieConsent from 'react-cookie-consent'
-import './volto-cookie-banner.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import CookieConsent from 'react-cookie-consent';
+import { flattenHTMLToAppURL } from '@plone/volto/helpers';
+import './volto-cookie-banner.css';
 
 const CookieBanner = ({ text, buttonText, acceptOnScroll = false }) => (
-  <CookieConsent buttonText={buttonText ?? 'OK'} acceptOnScroll={acceptOnScroll}>
-    <div dangerouslySetInnerHTML={{ __html: text }} />
+  <CookieConsent
+    buttonText={buttonText ?? 'OK'}
+    acceptOnScroll={acceptOnScroll}
+  >
+    <div dangerouslySetInnerHTML={{ __html: flattenHTMLToAppURL(text) }} />
   </CookieConsent>
-)
+);
 
 CookieBanner.propTypes = {
   text: PropTypes.string.isRequired,
   buttonText: PropTypes.string,
   acceptOnScroll: PropTypes.bool,
-}
+};
 
-export default CookieBanner
+export default CookieBanner;
